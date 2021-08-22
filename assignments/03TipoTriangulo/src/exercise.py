360f41038c31a4b5f2232d8c29973b15a311f2bd
valeriaaaen <A01740735@itesm.mx>
2021-08-22 03:57:43 +0000

    SES04-TA1

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -1,6 +1,29 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    x = int(input("Ingresa la medida del lado 1: "))
+    y = int(input("Ingresa la medida del lado 2: "))
+    z = int(input("Ingresa la medida del lado 3: "))
+
+    if not (x + y < z or x + z < y or y + z < x):
+        if x==y and y==z and z==x:
+            print("ES UN TRIANGULO EQUILATERO")
+        elif x!=y and y!=z and z!=x:
+            print("ES UN TRIANGULO ESCALENO") 
+        elif x==z and x!=y or x==y and x!=z or y==z and y!=x:
+            print("ES UN TRIANGULO ISOSCELES")
+    else:
+        print("NO ES TRIANGULO")
+
+        
+    
+   
+
+    
+
+    
+    
+        
+    
 
 if __name__=='__main__':
     main()
--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,25 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+    indice = peso / altura**2
+    if peso<=0 or altura<=0:
+        print("Revisa tus datos, alguno de ellos es erróneo.")
+    elif (indice)<20:
+        print("PESO BAJO")
+    elif (indice)<=25:
+        print("NORMAL")
+    elif (indice)<=30:
+        print("SOBREPESO")
+    elif (indice)<=40:
+        print("OBESIDAD")
+    elif (indice)>=40:
+        print("OBESIDAD MORBIDA")
+  
+
+    
+
+    
 
 if __name__=='__main__':
     main()
